0b52e4d30685ef1dbebe99fd7f8e6f61590c0d81
vindahpulotimahatv-commits <vindahpulotimahatv@gmail.com>
2026-09-23 11:07:06 +0700

    Add files via upload

---
+++ website/css/firebase.js
@@ -0,0 +1,26 @@
+// js/firebase.js
+// -----------------------------------------------------------
+// GANTI seluruh nilai di bawah ini dengan Firebase Config
+// milikmu sendiri. Kamu bisa mengambilnya di:
+// Firebase Console > Project Settings > General > Your apps > Web app
+// -----------------------------------------------------------
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "GANTI_DENGAN_API_KEY",
+  authDomain: "GANTI_DENGAN_AUTH_DOMAIN",
+  projectId: "GANTI_DENGAN_PROJECT_ID",
+  storageBucket: "GANTI_DENGAN_STORAGE_BUCKET",
+  messagingSenderId: "GANTI_DENGAN_SENDER_ID",
+  appId: "GANTI_DENGAN_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
